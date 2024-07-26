982a34012c129975fcf7c0a00669825b5239d01c
ArtistBanda <chirag.codes@gmail.com>
2024-07-26 22:32:29 +0530

    Setup files for ping pong server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", pingPong)
+
+	router.Run("localhost:8080")
+}
+
+func pingPong(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "pong")
+}
